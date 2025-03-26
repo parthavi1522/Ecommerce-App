c60e32ccee8e2ff326097646535594ec4a3ef17d
parthavi15 <parthavimd00@gmail.com>
2025-03-26 17:38:13 +0530

    Update admin dashboard page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,9 +3,10 @@ import "@popperjs/core"
 import "bootstrap"
 import "@hotwired/turbo-rails"
 import "controllers"
-
+import Rails from "rails-ujs";
 import { Application } from "@hotwired/stimulus"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
+Rails.start();
 const application = Application.start()
 eagerLoadControllersFrom("controllers", application)
