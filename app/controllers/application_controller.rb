635d1dd6a6efc2302de52de4345b7b763ca849c9
parthavi15 <parthavimd00@gmail.com>
2025-04-09 16:42:34 +0530

    Fixed customer signup issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :address, :mobile_number, :profile])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :address, :mobile_number, :profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :username, :address, :mobile_number, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :username, :address, :mobile_number, :profile])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,9 @@
 module Users
   class Users::RegistrationsController < Devise::RegistrationsController
+    protected
+
+    def after_sign_up_path_for(resource)
+      customer_dashboard_path
+    end
   end
 end
